97b7db1f422c88d940db1da9279aea2ebb85897e
Sandukith <sandukith.20191201@iit.ac.lk>
2021-03-30 16:16:32 +0530

    Digits Classification is 100% working

--- DataScience/OCR_CNN_Testing.py
+++ DataScience/OCR_CNN_Testing.py
@@ -1,48 +1,52 @@
 import numpy as np
 import cv2
 import pickle
+from tensorflow.python.keras.models import load_model
+
+###parameterrs###
+
+width = 640
+height = 480
+threshold = 0.65
+#threshold means minimum probability to classify
 
 
 
-width=640
-height=480
-threshold=0.65
-
-capture=cv2.VideoCapture(1)
+#this is the code for creatinng the camera objrct
+capture = cv2.VideoCapture(0)
 capture.set(3,width)
 capture.set(4,height)
 
+#here im loading the saved pretrained model
+model = load_model('model.h5')
 
-#model = keras.models.load_model("model_trained.p")
-pickle_in=open("trained_model.p","rb")
-model=pickle.load(pickle_in)
-
+#thid is the code for processing
 def preProcessing(img):
-    img-cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img=cv2.equalizeHist(img)
-    img=img/255
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    img = img/255
     return img
 
 while True:
-    success,imgOriginal=capture.read()
-    img=np.asarray(imgOriginal)
-    img=cv2.resize(img,(640,480))
-    img=preProcessing(img)
-    cv2.imshow(img)
-    cv2.imshow("Processed Image",img)
-    img=img.reshape(1,32,32,1)
-    #predict
-    classIndex=int(model.predict_classes(img))
-    print(classIndex)
-    predictions=model.predict(img)
-    probVal=np.amax(predictions)
+    success, imgOriginal = capture.read()
+    img = np.asarray(imgOriginal)
+    img=cv2.resize(img,(32,32))
+    img = preProcessing(img)
+    cv2.imshow("Processsed Image",img)
+    img = img.reshape(1, 32, 32, 1)
+    #prediction
+    classIndex = int(model.predict_classes(img))
 
-    if probVal>threshold:
-        cv2.putText(imgOriginal,str(classIndex)+" "+str(probVal),(50,50),cv2.FONT_HERSHEY_SIMPLEX,
-                    1,(0,0,255),1)
-        cv2.imshow("Original Image",imgOriginal)
+    predictions = model.predict(img)
 
-    k = cv2.waitKey(1) & 0xff
-    if k == 27:
+    probabilityValue = np.amax(predictions)
+    print(classIndex, probabilityValue)
+
+    if probabilityValue > threshold:
+        cv2.putText(imgOriginal, str(classIndex) + "   " + str(probabilityValue),
+                    (50, 50), cv2.FONT_HERSHEY_COMPLEX,
+                    1, (0, 0, 255), 1)
+
+    cv2.imshow("Testing Window", imgOriginal)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-capture.release()
--- DataScience/main.py
+++ DataScience/main.py
@@ -1,15 +1,8 @@
-import pickle
-
 import joblib
+import learn as learn
 import numpy as np
 import cv2
 import os
-
-import pandas
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
-import joblib
-
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
@@ -20,31 +13,26 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.layers import Dropout,Flatten
 from keras.layers.convolutional import Conv2D,MaxPooling2D
-import os
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+import dill
+import weakref
 
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.layers import Dense
-from keras.layers import Dropout, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.models import Sequential
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils.np_utils import to_categorical
-from sklearn.model_selection import train_test_split
 
-##############################
-path = 'myData'
+
+
+path = 'originalData'
 pathLabels = 'labels.csv'
 testRatio = 0.2
 validationRatio = 0.2
 imageDimensions = (32, 32, 3)
 
 batchSizeVal=50
-epochsVal=1
+epochsVal=5
 stepsPerEpoch=200
-#############################
+
 
 count = 0
 images = []
@@ -56,41 +44,40 @@ noOfClasses = len(mylist)
 print("Importing Classes...")
 # we will put all the images in a one list
 
-for filename in os.listdir(path):
-# for x in range(int(str("A")), noOfClasses):
-    myPicList = os.listdir(path + "/" + str(filename))
-    #print(myPicList)
-    # go in to mydata folder and then A folder and in that folder every photo you found must be added to the list
+
+for x in range(0, noOfClasses):
+    myPicList = os.listdir(path + "/" + str(x))
+    # go in to originalData folder and then 0 folder and in that folder every photo you found must be added to the list
     for y in myPicList:
-        curImg = cv2.imread(path + "/" + str(filename) + "/" + y)
-        # curImg = cv2.resize(curImg, (imageDimensions[0], imageDimensions[1]))
+        curImg = cv2.imread(path + "/" + str(x) + "/" + y)
+        curImg = cv2.resize(curImg, (imageDimensions[0], imageDimensions[1]))
         images.append(curImg)
         classNo.append(count)
 
     print(count, end=" ")
     count += 1
-print("")
 print("Total Images in image List = ", len(images))
 print("Total IDS in classNo List = ", len(classNo))
+# print(len(classNo))
+
 
 images = np.array(images)
 classNo = np.array(classNo)
 
 # splitting data
 x_train, x_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
-# your training is A.8 , testing is A.2
+# your training is 0.8 , testing is 0.2
 x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size=validationRatio)
-
 
 print("Training = ", x_train.shape)
 print("Testing = ", x_test.shape)
 print("Validation = ", x_validation.shape)
-'''in here what we are doing is we have 600 images. then it was split for testing training and validation
+'''in here what we are doing is we have 20320 images. then it was split for testing training and validation
 first we split all the data in to 80%(training) and 20%(Testing). then we again split that 80% in to 80% and 20%'''
 
 numOfSmaples = []
 for x in range(0, noOfClasses):
-    # print(len(np.where(y_train==x)[A]))
+    # print(len(np.where(y_train==x)[0]))
     numOfSmaples.append(len(np.where(y_train == x)[0]))
 print(numOfSmaples)
 
@@ -103,19 +90,18 @@ plt.ylabel("Number of Images")
 plt.show()
 
 
-
 def preProcessing(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.equalizeHist(img)  # makes the lights of the image equally
     img = img / 255
     return img
 
-'''we need to preprocess all the images in x_train array'''
 
-x_train = np.array(list(map(preProcessing(), x_train)))
+x_train = np.array(list(map(preProcessing, x_train)))
 x_test = np.array(list(map(preProcessing, x_test)))
 x_validation = np.array(list(map(preProcessing, x_validation)))
 
+print(x_train.shape)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 x_validation = x_validation.reshape(x_validation.shape[0], x_validation.shape[1], x_validation.shape[2], 1)
@@ -161,7 +147,6 @@ def myModel():
                   metrics=['accuracy'])
     return model
 
-
 model = myModel()
 print(model.summary())
 
@@ -192,21 +177,15 @@ score=model.evaluate(x_test,y_test,verbose=0)
 print('Test Score = ',score[0])
 print('Test Accuracy = ',score[1])
 
+#Save the model
+model.save("model.h5")
+print("Saved model to disk")
+model.summary()
 
-#model.save("A/model_trained.p",'wb')
-
-# filename = 'finalized_model.p'
-# joblib.dump(model, filename)
-
-###################
-pickle_out=open("trained_model.p",'wb')
-pickle.dump(model,pickle_out)
-pickle_out.close()
-
-# print(x_train.shape)
-# print(x_train[30].shape)
+print(x_train.shape)
+print(x_train[30].shape)
 
 # img = preProcessing(x_train[30])
-# img = cv2.resize(img, (300, 300))
-# cv2.imshow("PreProcessedImage",img)
-# cv2.waitKey(A)
+# # img = cv2.resize(img, (300, 300))
+# # cv2.imshow("PreProcessedImage",img)
+# # cv2.waitKey(0)
